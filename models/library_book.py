f245c8d3a8baaadc08b34b8ae9ebb68ea6febd03
ccysfn <jessecao911@gmail.com>
2019-06-13 10:20:25 +0800

    432

--- models/library_book.py
+++ models/library_book.py
@@ -10,3 +10,42 @@ class LibraryBook(models.Model):
         'res.partner',
         string='Authors'
     )
+    state = fields.Selection([
+         ('draft', 'Unavailable'),
+         ('available', 'Available'),
+         ('borrowed', 'Borrowed'),
+         ('lost', 'Lost')],
+         'State', default="draft")
+         
+    @api.model
+    def is_allowed_transition(self, old_state, new_state):
+        allowed = [('draft', 'available'),
+         ('available', 'borrowed'),
+         ('borrowed', 'available'),
+         ('available', 'lost'),
+         ('borrowed', 'lost'),
+         ('lost', 'available')]
+        return (old_state, new_state) in allowed
+        
+    @api.model
+    def is_allowed_transition(self, old_state, new_state):
+        allowed = [('draft', 'available'),
+         ('available', 'borrowed'),
+         ('borrowed', 'available'),
+         ('available', 'lost'), 
+         ('borrowed', 'lost'),
+         ('lost', 'available')]
+        return (old_state, new_state) in allowed
+        
+    @api.model
+    def make_available(self):
+        self.change_state('available')
+
+    @api.model
+    def make_borrowed(self):
+        self.change_state('borrowed')
+
+    @api.model
+    def make_lost(self):
+        self.change_state('lost')
+        
