fc67616e638d931a5c8527d682301bb993c4e744
ccysfn <jessecao911@gmail.com>
2019-06-13 10:30:15 +0800

    432

--- models/library_book.py
+++ models/library_book.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library.book'
+    
     name = fields.Char('Title', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many(
@@ -27,15 +28,13 @@ class LibraryBook(models.Model):
          ('lost', 'available')]
         return (old_state, new_state) in allowed
         
-    @api.model
-    def is_allowed_transition(self, old_state, new_state):
-        allowed = [('draft', 'available'),
-         ('available', 'borrowed'),
-         ('borrowed', 'available'),
-         ('available', 'lost'), 
-         ('borrowed', 'lost'),
-         ('lost', 'available')]
-        return (old_state, new_state) in allowed
+    @api.multi
+    def change_state(self, new_state):
+        for book in self:
+            if book.is_allowed_transition(book.state, new_state):
+                book.state = new_state
+            else:
+                continue
         
     @api.model
     def make_available(self):
